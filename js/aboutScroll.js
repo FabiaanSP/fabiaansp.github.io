062622633a0d0de79a2c7eda6189caa8e49d7dea
Fabiaan <51031189+FabiaanSP@users.noreply.github.com>
2022-01-28 21:46:21 +0100

    worked on the About Page

---
+++ js/aboutScroll.js
@@ -0,0 +1,58 @@
+let scrollIndex = 0;
+let maxScrollIndex = 2;
+let isAnimating = false;
+
+window.onwheel = e => {
+  if(e.deltaY >= 0){
+
+    // Scrolling Down
+
+    if(!isAnimating && scrollIndex < maxScrollIndex) {
+
+      console.log("Scroll Down");
+      isAnimating = true;
+      $('#box' + scrollIndex).addClass('animate__fadeOutUpBig');
+      setTimeout(function(){
+
+        document.getElementById("box" + scrollIndex).style.display = "none";
+        $('#box' + scrollIndex).removeClass('animate__fadeOutUpBig');
+        scrollIndex += 1;
+
+        document.getElementById("box" + scrollIndex).style.display = "block";
+        $('#box' + scrollIndex).removeClass('animate__fadeInDownBig');
+        $('#box' + scrollIndex).addClass('animate__fadeInUpBig');
+
+        setTimeout(function(){
+          isAnimating = false;
+          $('#box' + scrollIndex - 1).removeClass('animate__fadeInUpBig');
+        },1000)
+      },200)
+
+    }
+
+  } else {
+    // Scrolling Up
+
+    if (!isAnimating && scrollIndex > 0) {
+      console.log('Scroll Up');
+
+      isAnimating = true;
+      $('#box' + scrollIndex).addClass('animate__fadeOutDownBig');
+      setTimeout(function(){
+
+        document.getElementById("box" + scrollIndex).style.display = "none";
+        $('#box' + scrollIndex).removeClass('animate__fadeOutDownBig');
+        scrollIndex -= 1;
+
+        document.getElementById("box" + scrollIndex).style.display = "block";
+        $('#box' + scrollIndex).removeClass('animate__fadeInUpBig');
+        $('#box' + scrollIndex).addClass('animate__fadeInDownBig');
+
+        setTimeout(function(){
+          isAnimating = false;
+          $('#box' + scrollIndex - 1).removeClass('animate__fadeInDownBig');
+        },1000)
+      },200)
+    }
+  }
+}
