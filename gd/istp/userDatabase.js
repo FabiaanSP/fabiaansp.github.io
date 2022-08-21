7427288e0be81688cfd1dc5a4bc3082ce5a227c5
Fabiaan <fabi.sporn@gmail.com>
2022-08-21 16:35:57 +0200

    added Tim Flow

--- gd/istp/userDatabase.js
+++ gd/istp/userDatabase.js
@@ -1,2 +1,10 @@
-const Passcodes = ["1e94b5a3e45e52163564e5284ce0c098aa59fc93665331170b36c688"];
-const Usernames = ["Fabian Siedler"];
+const Passcodes = [
+    "1e94b5a3e45e52163564e5284ce0c098aa59fc93665331170b36c688", // Fabian Siedler
+    "dbd17b6d9029d4f4f7ebcf6e419de0a53940c0382702ee2702ae790a" // Tim Flow
+];
+
+
+const Usernames = [
+    "Fabian Siedler",
+    "Tim Flow"
+];
