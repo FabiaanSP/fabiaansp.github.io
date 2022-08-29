a2ef4fcbb51583df5b5a83993e6f7cf18dce7f79
Fabiaan <51031189+FabiaanSP@users.noreply.github.com>
2022-08-29 20:26:37 +0200

    added Liam James

--- gd/istp/userDatabase.js
+++ gd/istp/userDatabase.js
@@ -1,11 +1,13 @@
 const Passcodes = [
     "1e94b5a3e45e52163564e5284ce0c098aa59fc93665331170b36c688", // Fabian Siedler
-    "5164121b59272f135dc049f809b677655f513e0fc58dd6d57e860ae9" // Tim Flow
+    "5164121b59272f135dc049f809b677655f513e0fc58dd6d57e860ae9", // Tim Flow
+    "5a5f3eb2723da9c98d54f01363129a355a70ccd27dd311c73897c107" // Liam Jones
 ];
 
 const Usernames = [
     "Fabian Siedler",
-    "Tim Flow"
+    "Tim Flow",
+    "Liam Jones"
 ];
 
 // Damit Angreifer nicht einfach einen neuen Eintrag in die Datenbank einfügen können, werden die beiden
