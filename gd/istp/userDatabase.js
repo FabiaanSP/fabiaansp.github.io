e47a6ce52c5e8a2062c6c7dc8c01316ba6cf017a
Fabiaan <51031189+FabiaanSP@users.noreply.github.com>
2022-09-02 20:45:58 +0200

    added Version console Print

--- gd/istp/userDatabase.js
+++ gd/istp/userDatabase.js
@@ -1,3 +1,5 @@
+var Version = 1.10
+
 const Passcodes = [
     "1e94b5a3e45e52163564e5284ce0c098aa59fc93665331170b36c688", // Fabian Siedler
     "5164121b59272f135dc049f809b677655f513e0fc58dd6d57e860ae9", // Tim Flow
@@ -11,6 +13,8 @@ const Usernames = [
     "Liam Jones",
     "Marcus Draft"
 ];
+
+console.log(Version);
 
 // Damit Angreifer nicht einfach einen neuen Eintrag in die Datenbank einfügen können, werden die beiden
 // Arrays gefreezed, und sind somit nichtmehr veränderbar
